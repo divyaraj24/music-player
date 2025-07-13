3cfcef8e0dd4adc1f1a8b07224854406f167b104
divyaraj <divyarajdeepak2356@gmail.com>
2025-07-13 18:34:22 +0530

    Updated CMake and README

--- README.md
+++ README.md
@@ -2,6 +2,19 @@
 
 This project is for practicing c++ and its features.
 
+### Dependencies:
+
+Install zlib manually
+
+macOS:
+`brew install zlib`
+
+Linux (Arch):
+`sudo pacman -S zlib`
+
+Ubuntu/Debian:
+`sudo apt install zlib1g-dev`
+
 ### Goals:
 
 Features to add:
@@ -14,9 +27,8 @@ Features to add:
 - Delete song – From library or playlist
 - Save/load – Use file I/O to persist songs and playlists
 
-
 # 1. First Target:
- 
+
 For now, I will focus on creating a simple text based interface to add, display, search, play and pause.
 
 NOTES:
--- src/music_player.hpp
+++ src/music_player.hpp
@@ -4,40 +4,41 @@
 #include <string>
 #include <vector>
 
-class MusicPlayer {
-    public:
-        MusicPlayer(const std::string& filepath);
+class MusicPlayer
+{
+public:
+    MusicPlayer(const std::string &filepath);
 
-        enum class Status {
-            Stopped,
-            Paused,
-            Playing
-        };
+    enum class Status
+    {
+        Stopped,
+        Paused,
+        Playing
+    };
 
-        void Play();
-        void Pause();
-        void Stop();
-        void Previous();
-        void Next();
-        void VolumeUp(float val);
-        void VolumeDown(float val);
-        bool LoopSong(bool loop);
-        bool LoopPlaylist(bool loop);
+    void Play();
+    void Pause();
+    void Stop();
+    void Previous();
+    void Next();
+    void VolumeUp(float val);
+    void VolumeDown(float val);
+    bool LoopSong(bool loop);
+    bool LoopPlaylist(bool loop);
 
-        // Getting song, playlist and other information...
-        float getSongVolume() const;
-        Status getStatus() const;
-        Song getCurrentSong() const;
-        Song getNextSong() const;
-        Song getPreviousSong() const;
-        Playlist getPlaylist() const;
+    // Getting song, playlist and other information...
+    float getSongVolume() const;
+    Status getStatus() const;
+    Song getCurrentSong() const;
+    Song getNextSong() const;
+    Song getPreviousSong() const;
+    Playlist getPlaylist() const;
 
-    private:
-        std::vector<Playlist> m_playlists;
-        sf::Music m_music;
-        int m_playlistIndex = 0;
-        float m_volume = 100;
-        bool m_loopPlaylist = true;
-        bool m_loopSong = false;
+private:
+    std::vector<Playlist> m_playlists;
+    sf::Music m_music;
+    int m_playlistIndex = 0;
+    float m_volume = 60;
+    bool m_loopPlaylist = true;
+    bool m_loopSong = false;
 };
-
